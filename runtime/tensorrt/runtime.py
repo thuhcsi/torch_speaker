ed1e3acd7f5e3b4e24586f851ba78ceac682c03e
zyzisyz <zyziszy@foxmail.com>
2021-07-02 17:44:55 +0800

    update

--- runtime/tensorrt/runtime.py
+++ runtime/tensorrt/runtime.py
@@ -4,6 +4,8 @@ import pycuda.driver as cuda
 import pycuda.autoinit
 import tensorrt as trt
 
+TRT_LOGGER = trt.Logger()
+
 def load_engine(engine_file_path):
     assert os.path.exists(engine_file_path)
     print("Reading engine from file {}".format(engine_file_path))
@@ -11,5 +13,12 @@ def load_engine(engine_file_path):
         return runtime.deserialize_cuda_engine(f.read())
 
 def infer(engine, enroll_path, test_path):
+    # Create an execution context and specify input shape (based on the image dimensions for inference).
+    # Allocate CUDA device memory for input and output.
+    # Allocate CUDA page-locked host memory to efficiently copy back the output.
+    # Transfer the processed image data into input memory using asynchronous host-to-device CUDA copy.
+    # Kickoff the TensorRT inference pipeline using the asynchronous execute API.
+    # Transfer the segmentation output back into pagelocked host memory using device-to-host CUDA copy.
+    # Synchronize the stream used for data transfers and inference execution to ensure all operations are completes.
     pass
 
