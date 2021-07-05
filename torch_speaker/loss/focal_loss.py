889e8dc4467c05dfcc3ae0acaa73662f16df363c
zyzisyz <zyziszy@foxmail.com>
2021-07-05 17:22:45 +0800

    update

--- torch_speaker/loss/focal_loss.py
+++ torch_speaker/loss/focal_loss.py
@@ -4,16 +4,16 @@ import torch.nn.functional as F
 
 from .utils import accuracy
 
-class softmax(nn.Module):
-    def __init__(self, embedding_dim, num_classes, **kwargs):
-        super(softmax, self).__init__()
+class focal_loss(nn.Module):
+    def __init__(self, embedding_dim, num_classes, gamma=0.01, eps=1e-7, **kwargs):
+        super(focal_loss, self).__init__()
+        self.gamma = gamma
+        self.eps = eps
         self.embedding_dim = embedding_dim
         self.fc = nn.Linear(embedding_dim, num_classes)
         self.criertion = nn.CrossEntropyLoss()
 
-        print('Embedding dim is {}, number of speakers is {}'.format(embedding_dim, num_classes))
-
-    def forward(self, x, label=None):
+    def forward(self, x, label):
         assert len(x.shape) == 3
         label = label.repeat_interleave(x.shape[1])
         x = x.reshape(-1, self.embedding_dim)
@@ -21,14 +21,16 @@ class softmax(nn.Module):
         assert x.size()[1] == self.embedding_dim
 
         x = self.fc(x)
-        loss = self.criertion(x, label)
+ 
+        logp = self.criertion(x, label)
+        p = torch.exp(-logp)
+        loss = (1 - p) ** self.gamma * logp
         prec1 = accuracy(x.detach(), label.detach(), topk=(1,))[0]
         return loss, prec1
 
-
 if __name__ == "__main__":
-    model = softmax(10, 100)
-    data = torch.randn((2, 1, 10))
+    model = focal_loss(10, 100)
+    data = torch.ones((2, 1, 10))
     label = torch.tensor([0, 1])
     loss, acc = model(data, label)
 
