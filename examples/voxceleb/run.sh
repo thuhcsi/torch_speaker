01035f0b1f41424af12e533a42a284e508b180a1
zyzisyz <zyzisyz>
2021-07-07 21:38:38 +0800

    update

--- examples/voxceleb/run.sh
+++ examples/voxceleb/run.sh
@@ -5,7 +5,7 @@ voxceleb1_path=~/datasets/voxceleb/voxceleb1
 voxceleb2_path=~/datasets/voxceleb/voxceleb2
 musan_path=~/datasets/musan/
 
-stage=3
+stage=2
 
 if [ $stage -eq 0 ];then
 	rm -rf data/train/
@@ -56,29 +56,29 @@ if [ $stage -eq 3 ];then
     trial_path=data/vox1_clean.txt
 
     python3 scripts/make_cohort_set.py \
-        --data_list_path data/train.csv \
-        --cohort_save_path data/cohort.txt \
-        --num_cohort 10000
+	    --data_list_path data/train.csv \
+	    --cohort_save_path data/cohort.txt \
+	    --num_cohort 10000
 
-	python3 tools/evaluate.py \
-		--config config/voxceleb.yaml \
-		--trial_path $trial_path \
-		--checkpoint_path ckpt.pt
+    python3 tools/evaluate.py \
+	    --config config/voxceleb.yaml \
+	    --trial_path $trial_path \
+	    --checkpoint_path ckpt.pt
 
-	python3 tools/snorm_evaluate.py \
-		--config config/voxceleb.yaml \
-		--trial_path $trial_path \
-		--checkpoint_path ckpt.pt \
-        --score_save_path score.txt \
-        --cohort_path data/cohort.txt 
+    python3 tools/snorm_evaluate.py \
+	    --config config/voxceleb.yaml \
+	    --trial_path $trial_path \
+	    --checkpoint_path ckpt.pt \
+	    --score_save_path score.txt \
+	    --cohort_path data/cohort.txt 
 
     python3 tools/asnorm_evaluate.py \
-        --config config/voxceleb.yaml \
-        --trial_path data/vox1.txt \
-        --checkpoint_path ckpt.pt \
-        --score_save_path score.txt \
-        --cohort_path data/cohort.txt \
-        --top_n 300
+	    --config config/voxceleb.yaml \
+	    --trial_path data/vox1.txt \
+	    --checkpoint_path ckpt.pt \
+	    --score_save_path score.txt \
+	    --cohort_path data/cohort.txt \
+	    --top_n 256
 fi
 
 if [ $stage -eq 4 ];then
